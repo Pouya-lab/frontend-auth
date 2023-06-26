ac392e2257b21597a9d1b40938eb21af7ad2006c
pouya <pouyabh102@gmail.com>
2023-06-26 16:16:41 +0330

    basics are ready for frontend

---
+++ app/basket/page.js
@@ -0,0 +1,13 @@
+
+
+function Basket() {
+  return (
+    <div>
+        <h1 className="new" >
+        Basket
+        </h1>
+    </div>
+  )
+}
+
+export default Basket
---
+++ app/home/page.js
@@ -0,0 +1,13 @@
+
+
+function Home() {
+  return (
+    <>
+        <h1 className="bg-slate-400" >        
+            welcome to home!!!
+        </h1>
+    </>
+  )
+}
+
+export default Home
