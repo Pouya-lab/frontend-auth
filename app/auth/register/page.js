37444d27fde9dfc1ba323c56895e9e43211725c6
pouya <pouyabh102@gmail.com>
2023-06-27 21:28:08 +0330

    password strength body for icons and etc has been made

--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -3,9 +3,11 @@
 import Card from '@/components/card/page'
 import PasswordInput from '@/components/passwordInput/page'
 import {TiUserAddOutline} from 'react-icons/ti'
+import { BsCheck2All } from 'react-icons/bs';
 import {React , useState} from 'react';
 import Link from 'next/link';
 import styles from '../auth.module.scss'
+import { FaTimes } from 'react-icons/fa';
 //for using states we need to import both state and react and also add use client at the top of our page
 
 
@@ -22,8 +24,26 @@ function Register() {
     const {name , email , password , password2 } = formData
     //code above helps us to write name and other properties easier than formData.name , formData.email and .....
 
-    const handleInputeChange = () =>{
+    //for checking passwords strength
+    const [upperCase, setUpperCase] = useState(false)
+    const [num, setNum] = useState(false)
+    const [specialChar, setSpecialChar] = useState(false)
+    const [passLenght, setPassLenght] = useState(false)
+    //icons for checking passwords strength
+    const timesIcon = <FaTimes size={15} color="red"/>;
+    const checkIcon = <BsCheck2All size={15} color='green' />
 
+    const switchIcon = (condition)=>{
+        if(condition){
+            return checkIcon
+        }
+        return timesIcon
+    }
+
+    //function below helps us to set value inside input
+    const handleInputeChange = (e) =>{
+         const {name , value} = e.target
+         setFormData({ ...formData , [name] : value })
     }
 
     const loginUser = () =>{
@@ -78,6 +98,37 @@ function Register() {
                             value={password2}
                             onChange={handleInputeChange}
                         />
+
+                        {/* password strenght */}
+                        <Card cardClass = {styles.group}>
+                            <ul className='form-list' >
+                                <li>
+                                    <span className={styles.indicator}>
+                                        {switchIcon(upperCase)}
+                                        &nbsp; LowerCase and UpperCase
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className={styles.indicator}>
+                                        {switchIcon(num)}
+                                        &nbsp; Number 0-9
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className={styles.indicator}>
+                                        {switchIcon(specialChar)}
+                                        &nbsp; Special Character #$%*&
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className={styles.indicator}>
+                                        {switchIcon(passLenght)}
+                                        &nbsp; At least 6 Character
+                                    </span>
+                                </li>
+                            </ul>
+                        </Card>
+
                         <button type='submit' className='--btn --btn-primary w-full' >
                             Register
                         </button>
