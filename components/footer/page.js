6472e1619b3491e26e62afde1269ebcbfad06ff1
pouya <pouyabh102@gmail.com>
2023-06-27 11:51:08 +0330

    footer and header has been created

---
+++ components/footer/page.js
@@ -0,0 +1,20 @@
+
+
+
+
+function Footer() {
+
+
+  return (
+    <>
+      <hr className="--color-dark" />
+      <div className=" --py2 --bg-grey --flex-center">
+          <p>
+            All Reserverd and done!!!!
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ components/header/page.js
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import '../../styles/vendor/header.scss'
+import {BiLogIn} from 'react-icons/bi'
+import {FaUserCircle} from 'react-icons/fa'
+
+
+ 
+
+function Header() {
+  
+
+
+  return (
+    <>
+      <header className='header'>
+        <nav className='' >
+
+          <Link className='logo' href="/" >
+                <BiLogIn className='--color-white' size={90} />
+                <span className=' --color-white'>Pouya AUTH</span>       
+          </Link>
+
+          <ul className='home-links '>
+            <li className='--flex-center' >
+              <FaUserCircle className="--color-white" size={40} />
+              <p className="--color-white">
+                Hi , Pouya
+              </p>
+            </li>
+            <li>
+              <button className='--btn --btn-primary'>
+                  <Link href='#' >Login</Link>
+              </button>
+            </li>
+            <li className='--btn --btn-primary'>
+              <Link href='#' >Profile</Link>
+            </li>
+            <li>
+              <button className='--btn --btn-secondary'>
+                  Logout
+              </button>
+            </li>
+          </ul>
+
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
